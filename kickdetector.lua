1b75736380ef0fe5f6b5d502e01212f7060dac70
RoseGoldIsntGay <yoavkau@gmail.com>
2022-08-27 23:37:12 +0300

    different detection method

--- kickdetector.lua
+++ kickdetector.lua
@@ -1,7 +1,12 @@
-local connection = game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(child)
-    print(child)
-    if child and child.Name == 'ErrorPrompt' and child:FindFirstChild('MessageArea') and child.MessageArea:FindFirstChild("ErrorFrame") then
-        game:Shutdown()
+local doneloading = false
+
+task.spawn(function()
+    while not doneloading do
+        task.wait()
+        local ep = game:GetService("CoreGui").RobloxPromptGui.promptOverlay:FindFirstChild("ErrorPrompt")
+        if ep and ep:FindFirstChild('MessageArea') and ep.MessageArea:FindFirstChild("ErrorFrame") then
+            game:Shutdown()
+        end
     end
 end)
 
@@ -10,4 +15,4 @@ repeat task.wait(0.1) until player.PlayerGui.ScreenGui:FindFirstChild("Menus")
 
 print("Game was properly loaded, so now the main Rosemoc file will handle any disconnections")
 
-connection:Disconnect()
+doneloading = true
