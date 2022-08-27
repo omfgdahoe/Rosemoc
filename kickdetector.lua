b94604dcc9d53605a5b96ee8939ce73aec02494e
RoseGoldIsntGay <yoavkau@gmail.com>
2022-08-27 23:32:13 +0300

    Update kickdetector.lua

--- kickdetector.lua
+++ kickdetector.lua
@@ -1,5 +1,6 @@
 local connection = game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(child)
-    if child.Name == 'ErrorPrompt' and child:FindFirstChild('MessageArea') and child.MessageArea:FindFirstChild("ErrorFrame") then
+    print(child)
+    if child and child.Name == 'ErrorPrompt' and child:FindFirstChild('MessageArea') and child.MessageArea:FindFirstChild("ErrorFrame") then
         game:Shutdown()
     end
 end)
